510d0d18a908028a895ddef0993fc0dce135375d
tee-sherman <tee.sherman.wp@gmail.com>
2023-01-01 16:55:18 -0600

    added html and js files to new branch

---
+++ simpleJsFunction/index.js
@@ -0,0 +1,11 @@
+
+
+function nameHeightCountry (firstName, height, country){
+    alert(firstName +height  + country);
+  };
+  
+  nameHeightCountry('first name: Tee\n','height: 5ft\n','Country: United States\n');
+  
+  
+  
+  
